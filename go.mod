3161c22f5ddcdbf76a5d2b392b11868b4bff648b
tuedt <v.tuedt1@vinid.net>
2021-01-26 11:23:22 +0700

    v0.0.1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hieua1/test
+
+go 1.14
+
+require github.com/hieua1/testroot v0.0.2 // indirect
---
+++ pkg1.go
@@ -0,0 +1,7 @@
+package test
+
+import "github.com/hieua1/testroot"
+
+func Trans1(s string) string {
+	return testroot.Transform(s)
+}
